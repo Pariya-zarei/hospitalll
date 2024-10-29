cd7159331bd5f337977d7b8d713cdea9a7b51dfe
Pariya.Zarei <art0pariya0z@gmail.com>
2024-10-29 13:02:53 +0330

    Update views.py

--- reserve/views.py
+++ reserve/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from .serializer import *
+from rest_framework.generics import ListCreateAPIView,RetrieveUpdateDestroyAPIView, ListAPIView
+from rest_framework.permissions import IsAuthenticated
+from .models import Reserve
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 
-# Create your views here.
+class ReserveView(ListCreateAPIView):
+    queryset = Reserve.objects.all()
+    serializer_class = ReserveSerializer
+    permission_classes = [IsAuthenticated]
+
+    
+class ReserveDetail(RetrieveUpdateDestroyAPIView):
+    queryset = Reserve.objects.all()
+    serializer_class = ReserveSerializer
+    permission_classes = [IsAuthenticated]
+
+    
+class ReserveListView(ListAPIView):
+    queryset = Reserve.objects.all()
+    serializer_class = ReserveListSerializer
+    permission_classes = [IsAuthenticated]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['date','time']
