fccb15f0d7ece674b151e0e6404ab207456b6178
Pariya.Zarei <art0pariya0z@gmail.com>
2024-10-29 13:03:30 +0330

    Update views.py

--- service/views.py
+++ service/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from .serializer import *
+from .models import *
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView, ListAPIView
+from rest_framework.permissions import IsAuthenticated
+from .permissions import *
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 
-# Create your views here.
+
+class ServiceView(ListCreateAPIView):
+    queryset = Service.objects.all()
+    serializer_class = ServiceSerializer
+    permission_classes = [IsAuthenticated]
+
+
+
+class ServiceDetail(RetrieveUpdateDestroyAPIView):
+    queryset = Service.objects.all()
+    serializer_class = ServiceSerializer
+    permission_classes = [IsAuthenticated]
+
+
+
+class SuperUserView(ListAPIView):
+    queryset = Reserve.objects.all()
+    serializer_class = SuperUserReserveSerializer
+    permission_classes = [IsSuperUser]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['date']
+
+
+    
